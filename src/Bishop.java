9bb2f18700b1b61c6b7adad8476edcda28fd4061
Anca <anca.lumezeanu@gmail.com>
2023-09-08 18:49:02 +0100

    Added validate in between positions for bishop and queen

--- src/Bishop.java
+++ src/Bishop.java
@@ -14,12 +14,36 @@ public class Bishop extends Piece {
         super.move(newPosition, BOARD);
     }
 
+    private boolean validateInBetweenPositions(Position newPosition, Piece[][] BOARD) {
+        for (int i = 1; i <= Math.abs(getCurrentPosition().getColumn() - newPosition.getColumn()); i++) {
+            if (getCurrentPosition().getRow() > newPosition.getRow() && getCurrentPosition().getColumn() > newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() - i][getCurrentPosition().getColumn() - i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() > newPosition.getRow() && getCurrentPosition().getColumn() < newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() - i][getCurrentPosition().getColumn() + i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() < newPosition.getRow() && getCurrentPosition().getColumn() < newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() + i][getCurrentPosition().getColumn() + i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() < newPosition.getRow() && getCurrentPosition().getColumn() > newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() + i][getCurrentPosition().getColumn() - i] != null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     @Override
     public boolean validateMove(Position newPosition, Piece[][] BOARD) {
         int rowDifference = Math.abs(newPosition.getRow() - getCurrentPosition().getRow());
         int columnDifference = Math.abs(newPosition.getColumn() - getCurrentPosition().getColumn());
 
-        return super.validateMove(newPosition, BOARD) && rowDifference == columnDifference;
+        return super.validateMove(newPosition, BOARD) && rowDifference == columnDifference && validateInBetweenPositions(newPosition, BOARD);
     }
 
     @Override
--- src/Queen.java
+++ src/Queen.java
@@ -14,12 +14,63 @@ public class Queen extends Piece {
         super.move(newPosition, BOARD);
     }
 
+    private boolean validateInBetweenPositionsRook(Position newPosition, Piece[][] BOARD) {
+        if (getCurrentPosition().getColumn() != newPosition.getColumn()) {
+            int columnDifference = getCurrentPosition().getColumn() - newPosition.getColumn();
+            int minColumn = Math.min(newPosition.getColumn(), getCurrentPosition().getColumn());
+
+            for (int i = 0; i < Math.abs(columnDifference); i++) {
+                if (BOARD[newPosition.getRow()][minColumn + i + 1] != null) {
+                    return false;
+                }
+            }
+        } else {
+            int rowDifference = getCurrentPosition().getRow() - newPosition.getRow();
+            int minRow = Math.min(newPosition.getRow(), getCurrentPosition().getRow());
+
+            for (int i = 0; i < Math.abs(rowDifference); i++) {
+                if (BOARD[minRow + i + 1][newPosition.getColumn()] != null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean validateInBetweenPositionsBishop(Position newPosition, Piece[][] BOARD) {
+        for (int i = 1; i <= Math.abs(getCurrentPosition().getColumn() - newPosition.getColumn()); i++) {
+            if (getCurrentPosition().getRow() > newPosition.getRow() && getCurrentPosition().getColumn() > newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() - i][getCurrentPosition().getColumn() - i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() > newPosition.getRow() && getCurrentPosition().getColumn() < newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() - i][getCurrentPosition().getColumn() + i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() < newPosition.getRow() && getCurrentPosition().getColumn() < newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() + i][getCurrentPosition().getColumn() + i] != null) {
+                    return false;
+                }
+            } else if (getCurrentPosition().getRow() < newPosition.getRow() && getCurrentPosition().getColumn() > newPosition.getColumn()) {
+                if (BOARD[getCurrentPosition().getRow() + i][getCurrentPosition().getColumn() - i] != null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     @Override
     public boolean validateMove(Position newPosition, Piece[][] BOARD) {
         int rowDifference = Math.abs(newPosition.getRow() - getCurrentPosition().getRow());
         int columnDifference = Math.abs(newPosition.getColumn() - getCurrentPosition().getColumn());
 
-        return super.validateMove(newPosition, BOARD) && ((rowDifference == columnDifference) || (newPosition.getRow() == getCurrentPosition().getRow() || newPosition.getColumn() == getCurrentPosition().getColumn()));
+        return super.validateMove(newPosition, BOARD) && (validateInBetweenPositionsRook(newPosition, BOARD) ||
+                validateInBetweenPositionsBishop(newPosition, BOARD)) &&
+                ((rowDifference == columnDifference) || (newPosition.getRow() == getCurrentPosition().getRow() ||
+                        newPosition.getColumn() == getCurrentPosition().getColumn())) ;
 
     }
 
