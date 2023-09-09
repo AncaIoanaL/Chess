a5501377a182eec3cb429f7ac692782013fbb38c
Anca <anca.lumezeanu@gmail.com>
2023-09-09 10:32:37 +0100

    Finished promote pawn move

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Pawn extends Piece {
 
     private final Position initialPosition;
@@ -8,20 +10,18 @@ public class Pawn extends Piece {
     }
 
     @Override
-    public void attack() {
-
+    public void move(Position newPosition, Piece[][] BOARD) {
+        if (shouldPromotePawn(newPosition, BOARD)) {
+            BOARD[newPosition.getRow()][newPosition.getColumn()] = promotePawn(newPosition);
+            BOARD[getCurrentPosition().getRow()][getCurrentPosition().getColumn()] = null;
+        } else {
+            super.move(newPosition, BOARD);
+        }
     }
 
-    private boolean validateInBetweenPositions(Piece[][] BOARD) {
-        if (getCurrentPosition().equals(initialPosition)) {
-            if (Colour.BLACK.equals(getColour())) {
-                return BOARD[getCurrentPosition().getRow() + 1][getCurrentPosition().getColumn()] == null;
-            } else {
-                return BOARD[getCurrentPosition().getRow() - 1][getCurrentPosition().getColumn()] == null;
-            }
-        }
+    @Override
+    public void attack() {
 
-        return true;
     }
 
     @Override
@@ -33,6 +33,11 @@ public class Pawn extends Piece {
         }
 
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Pawn{" + getColour() + "}";
     }
 
     private boolean validateMove(Position newPosition) {
@@ -49,6 +54,18 @@ public class Pawn extends Piece {
         }
     }
 
+    private boolean validateInBetweenPositions(Piece[][] BOARD) {
+        if (getCurrentPosition().equals(initialPosition)) {
+            if (Colour.BLACK.equals(getColour())) {
+                return BOARD[getCurrentPosition().getRow() + 1][getCurrentPosition().getColumn()] == null;
+            } else {
+                return BOARD[getCurrentPosition().getRow() - 1][getCurrentPosition().getColumn()] == null;
+            }
+        }
+
+        return true;
+    }
+
     private boolean validateAttack(Position newPosition) {
         int moveBy = Colour.BLACK.equals(getColour()) ? 1 : -1;
 
@@ -56,26 +73,36 @@ public class Pawn extends Piece {
                 (newPosition.getColumn() == getCurrentPosition().getColumn() + 1 || newPosition.getColumn() == getCurrentPosition().getColumn() - 1);
     }
 
-    private boolean validatePawnPromotion(Position newPosition, Piece[][] BOARD) {
-        if (Colour.WHITE.equals(BOARD[getCurrentPosition().getRow()][getCurrentPosition().getColumn()].getColour())) {
-            for (int i = 0; i <= 7; i ++) {
-                if (newPosition.getRow() == 0 && newPosition.getColumn() == i) {
-                    return true;
-                }
-            }
-        } else if (Colour.BLACK.equals(BOARD[getCurrentPosition().getRow()][getCurrentPosition().getColumn()].getColour())) {
-            for (int i = 0; i <= 7; i ++) {
-                if (newPosition.getRow() == 7 && newPosition.getColumn() == i) {
-                    return true;
-                }
-            }
+    private boolean shouldPromotePawn(Position newPosition) {
+        if (Colour.BLACK.equals(getColour())) {
+            return newPosition.getRow() == 7;
+        } else {
+            return newPosition.getRow() == 0;
         }
-
-        return false;
     }
 
-    @Override
-    public String toString() {
-        return "Pawn{" + getColour() + "}";
+    private Piece promotePawn(Position newPosition) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Pawn is valid for promotion, please choose the piece it will be promoted to: ");
+            String promotionPiece = scanner.nextLine();
+
+            switch (promotionPiece) {
+                case "QUEEN" -> {
+                    return new Queen(getColour(), newPosition);
+                }
+                case "ROOK" -> {
+                    return new Rook(getColour(), newPosition);
+                }
+                case "BISHOP" -> {
+                    return new Bishop(getColour(), newPosition);
+                }
+                case "KNIGHT" -> {
+                    return new Knight(getColour(), newPosition);
+                }
+                default -> System.out.println("Input is invalid. Should be QUEEN, ROOK, BISHOP or KNIGHT");
+            }
+        }
     }
 }
